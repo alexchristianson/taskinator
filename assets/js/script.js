5144f21018b7b6e225ad8f33348a6fba53dd2b52
Alex C <alex.christianson.05@gmail.com>
2021-09-16 10:52:27 -0500

    load saved tasks and refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,12 +25,6 @@ var taskFormHandler = function(event) {
 
     var isEdit = formEl.hasAttribute("data-task-id");
 
-    // // package up data as an object
-    // var taskDataObj = {
-    //     name: taskNameInput,
-    //     type: taskTypeInput
-    // };
-
     // has data attribute, so get task id and call function to complete edit process
     if (isEdit) {
         var taskId = formEl.getAttribute("data-task-id");
@@ -235,7 +229,26 @@ var taskButtonHandler = function(event) {
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+};
+
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
+    return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+    createTaskEl(savedTasks[i]);
+    }
+};
+
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
